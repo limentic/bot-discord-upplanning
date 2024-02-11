125dacd2f8f4b41f9dfadea8c16105dd982b86c7
Limentic <limentic@hotmail.com>
2024-02-11 15:48:05 +0100

    Fixed a bug with scheduled_refresh.sh

--- scheduled_refresh.sh
+++ scheduled_refresh.sh
@@ -10,7 +10,7 @@ perform_curl_with_retry() {
   for ((i = 1; i <= max_retries; i++)); do
     response=$(curl -sS "$url" 2>&1)
 
-    if [[ "$url" == *"calendar/haschanged"* && ($response == *"\"hasChanged\":false"* || $response == *"\"hasChanged\":true"*) ]]; then
+    if [[ "$url" == *"calendar"* && ($response == *"\"hasChanged\":false"* || $response == *"\"hasChanged\":true"*) ]]; then
       logger -t "$log_tag" "Curl successful for $url"
       return 0  # Success
     else
